a91494e5d1fe73bb6f26520522219bef67b04f44
sultan alam <sultancodess@gmail.com>
2025-12-19 23:22:03 +0530

    dd

--- DEV/JavaScript/index.js
+++ DEV/JavaScript/index.js
@@ -1,8 +1,1 @@
 console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
-console.log("hello world");
