d0bc168dab064c5ce113183ea1c6a9d414aab985
sultanxdev <sultancodess@gmail.com>
2026-01-01 16:34:08 +0530

    some array question practices

---
+++ DSA/Array/01_print_array.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n;
+    cout<<"Enter size of array: ";
+    cin>>n;
+    int arr[n];
+    cout<<"Enter elements of array: ";
+
+    for(int i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    cout<<"Elements of array are: ";
+    for(int i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+    return 0;
+}
